f6fafcd47940e808f828e6782aa4623b5b14d11e
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-01-21 15:56:10 -0500

    up

--- vcf2gds.R
+++ vcf2gds.R
@@ -10,7 +10,7 @@ vcf.fn=args[[1]]
 gds.fn=gsub(".vcf", ".gds", vcf.fn)
 
 vcf.fn=paste(vcf.fn, ".gz", sep="")
-vcf.fn="/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Bams/vcf/2022seq.concat.Removereps.vcf"
+#vcf.fn="/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Bams/vcf/2022seq.concat.Removereps.vcf.gz"
 seqParallelSetup(cluster=10, verbose=TRUE)
 
 seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA", parallel=10, verbose=T, optimize=T)
