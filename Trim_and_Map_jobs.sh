8ece6f8f243c4882910ec740cdb9174926b7e57c
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-01-20 19:30:16 -0500

    push

--- Trim_and_Map_jobs.sh
+++ Trim_and_Map_jobs.sh
@@ -3,10 +3,42 @@
 
 INPUT_FILES=("/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Gilmer5_A1")  # List of input file prefixes (without extensions)
 
-chmod -x Trim_and_Map.sh
+chmod -x Trim_and_map.sh
 
 # Loop through each input file and submit a job
 for INPUT in "${INPUT_FILES[@]}"; do 
     sbatch --job-name=TrimMap_${INPUT} \
            Trim_and_map.sh ${INPUT}
 done
+
+sacct -j 1466105
+
+
+
+
+
+
+L6_1=$( ls ${dir}/*L6_1.fq.gz )
+L6_2=$( ls ${dir}/*L6_2.fq.gz )
+
+## adapter removal
+
+#### adapter removal and inital mapping
+### run for L3
+cutadapt \
+-q 18 \
+--minimum-length 75 \
+-o ${L6_1}.trimmed1.fq.gz \
+-p ${L6_2}.trimmed2.fq.gz \
+-O 15 \
+-n 3 \
+--cores=10 \
+${L6_1} ${L6_2}
+
+bwa mem \
+-t 10 \
+-R "@RG\tID:${dir}_L6\tSM:sample_name\tPL:illumina\tLB:lib1" \
+/project/berglandlab/daphnia_ref/totalHiCwithallbestgapclosed.fa \
+${L6_1} ${L6_2} |
+samtools view -@ 10 -Sbh -q 20 -F 0x100 - > ${dir}.L6.bam
+### samtools view -@ 10 -Sbh -q 20 -F 0x100 - > ${dir}/${dir}.L6.bam
--- Trim_and_map.sh
+++ Trim_and_map.sh
@@ -3,15 +3,14 @@
 #SBATCH -J download # A single job name for the array
 #SBATCH --ntasks-per-node=10 # ten cores
 #SBATCH -N 1 # on one node
-#SBATCH -t 8:00:00 ### 8 hours
+#SBATCH -t 16:00:00 ### 8 hours
 #SBATCH --mem 60G
 #SBATCH -o /scratch/rjp5nc/download_%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/download_%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-module load cutadapt gcc/11.4.0 bwa/0.7.17 samtools/1.17 java/11
-java -jar $EBROOTPICARD/picard.jar
+module load cutadapt gcc/11.4.0 bwa/0.7.17 samtools/1.17 picard/2.27.5
 
 # sbatch --array=2 ~/Genoseq_2023/Trim_and_map.sh
 ### sacct -j 40641767
@@ -22,35 +21,20 @@ java -jar $EBROOTPICARD/picard.jar
 # SLURM_ARRAY_TASK_ID=1
 
 dir=/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Gilmer5_A1
-L6_1=$( ls ${dir}/*L6_1.fq.gz )
-L6_2=$( ls ${dir}/*L6_2.fq.gz )
 
-## adapter removal
 
-#### adapter removal and inital mapping
-### run for L3
-cutadapt \
--q 18 \
---minimum-length 75 \
--o ${L6_1}.trimmed1.fq.gz \
--p ${L6_2}.trimmed2.fq.gz \
--O 15 \
--n 3 \
---cores=10 \
-${L6_1} ${L6_2}
 
-bwa mem \
--t 10 \
--R "@RG\tID:${dir}_L6\tSM:sample_name\tPL:illumina\tLB:lib1" \
-/project/berglandlab/daphnia_ref/totalHiCwithallbestgapclosed.fa \
-${L6_1} ${L6_2} |
-samtools view -@ 10 -Sbh -q 20 -F 0x100 - > ${dir}.L6.bam
-### samtools view -@ 10 -Sbh -q 20 -F 0x100 - > ${dir}/${dir}.L6.bam
+
+
+java -jar $EBROOTPICARD/picard.jar SortSam \
+-I ${dir}.L6.bam \
+-O ${dir}.L6.sorted.bam \
+-SORT_ORDER coordinate
 
 ### PCR duplicate removal
 java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
 -REMOVE_DUPLICATES true \
--I ${dir}.L6.bam \
+-I ${dir}.L6.sorted.bam \
 -O ${dir}.sort.dedup.bam \
 -M ${dir}.mark_duplicates_report.txt \
 -VALIDATION_STRINGENCY SILENT
