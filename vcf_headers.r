7a7a885233abf79750d697c57e363859008781b4
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-01-21 19:27:51 -0500

    Update vcf_headers.r

--- vcf_headers.r
+++ vcf_headers.r
@@ -1,10 +1,22 @@
 
 library(vcfR)
+library(adegenet)
+library(ape)
 
-vcf_file <- "/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Bams/vcf/2022seq.concat.Removereps.vcf.gz"
-vcf <- read.vcfR(vcf_file)
 
-sample_names <- colnames(vcf@gt)[-c(1:9)]
 
-write.csv(sample_names,"/project/berglandlab/Robert/samps.csv")
+vcf <- read.vcfR("/project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Bams/vcf/2022seq.concat.Removereps.vcf.gz")
+
+# Step 2: Extract Genotype Data
+genotypes <- extract.gt(vcf)
+
+# Step 3: Convert Genotype Data to Genind Object
+genind_obj <- vcfR2genind(vcf)
+
+# Step 4: Compute Distance Matrix
+dist_matrix <- dist.genpop(genind_obj)
+
+# Step 5: Build Phylogenetic Tree (Neighbor-Joining)
+tree <- nj(dist_matrix)
+save(tree, file = "/project/berglandlab/Robert/UKSequencing2022_2024/phylogenetic_tree.RData")
 
