e9a05fb9ce3e06a3b1041fa8b99de935094fbda3
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-01-31 20:53:59 -0500

    Update Canu.sh

--- ElvisLongRead/Canu.sh
+++ ElvisLongRead/Canu.sh
@@ -37,7 +37,7 @@ canu \
  -d dap_hifi_trim \
  maxThreads=30 \
  maxMemory=100g \
- useGrid=true \
+ useGrid=false \
  genomeSize=150m \
  gnuplot=/home/rjp5nc/miniconda3/bin/gnuplot \
  -pacbio-raw m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz
