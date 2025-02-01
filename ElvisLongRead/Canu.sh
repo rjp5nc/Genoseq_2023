415219a628fd5159764011b5f599861d7d2fe144
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-01-31 20:53:45 -0500

    up

--- ElvisLongRead/Canu.sh
+++ ElvisLongRead/Canu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J run_hicanu # A single job name for the array
-#SBATCH --ntasks-per-node=40 # multi core
+#SBATCH --ntasks-per-node=30 # multi core
 #SBATCH -N 1 # on one node
 #SBATCH -t 5-00:00 # 5 days
-#SBATCH --mem 360G
+#SBATCH --mem 150G
 #SBATCH -o /scratch/rjp5nc/run_hiCanu1.out # Standard output
 #SBATCH -e /scratch/rjp5nc/run_hiCanu1.err # Standard error
 #SBATCH -p standard
@@ -35,9 +35,9 @@ canu \
 -assemble \
 -p dap \
  -d dap_hifi_trim \
- maxThreads=40 \
- maxMemory=360g \
- useGrid=false \
+ maxThreads=30 \
+ maxMemory=100g \
+ useGrid=true \
  genomeSize=150m \
  gnuplot=/home/rjp5nc/miniconda3/bin/gnuplot \
  -pacbio-raw m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz
