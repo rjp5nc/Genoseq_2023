c36bafacedbaef034c52c3b016567ce5fea76863
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-01 22:04:48 -0500

    up

--- ElvisLongRead/Canu.sh
+++ ElvisLongRead/Canu.sh
@@ -3,7 +3,7 @@
 #SBATCH -J run_hicanu # A single job name for the array
 #SBATCH --ntasks-per-node=20 # multi core
 #SBATCH -N 1 # on one node
-#SBATCH -t 3-00:00 # 5 days
+#SBATCH -t 4-00:00 # 5 days
 #SBATCH --mem 100G
 #SBATCH -o /scratch/rjp5nc/run_hiCanu1.out # Standard output
 #SBATCH -e /scratch/rjp5nc/run_hiCanu1.err # Standard error
@@ -38,14 +38,8 @@ canu \
  maxThreads=30 \
  maxMemory=150g \
  useGrid=true \
- gridOptionsArray="--array=1-88" \
  gridOptions="--partition=standard --account=berglandlab --time=1-00:00 --mem=100G --cpus-per-task=20" \
- gridOptionsBogart="--mem=60G --cpus-per-task=16" \
- gridOptionsMhap="--mem=20G --cpus-per-task=8" \
- gridOptionsOvl="--mem=15G --cpus-per-task=4" \
- gridOptionsMinimap="--mem=10G --cpus-per-task=4" \
- gridOptionsCor="--mem=30G --cpus-per-task=8" \
- gridOptionsCns="--mem=25G --cpus-per-task=8" \ 
+ gridOptionsArray="--array=1-88" \
  genomeSize=150m \
  gnuplot=/home/rjp5nc/miniconda3/bin/gnuplot \
  -pacbio-raw m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz
