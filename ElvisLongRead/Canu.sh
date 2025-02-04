8e7d2de460ab2666da1d30d81c8eebdaeab65230
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-03 20:38:37 -0500

    Update Canu.sh

--- ElvisLongRead/Canu.sh
+++ ElvisLongRead/Canu.sh
@@ -37,12 +37,13 @@ canu \
  -d dap_hifi_trim \
  maxThreads=30 \
  maxMemory=150g \
- useGrid=true \
- gridOptions="--partition=standard --account=berglandlab --time=2-00:00"\
- gridEngineArrayMaxJobs=5 \
+ useGrid=false \
  genomeSize=150m \
  gnuplot=/home/rjp5nc/miniconda3/bin/gnuplot \
  -pacbio-raw m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz
+
+### gridOptions="--partition=standard --account=berglandlab --time=2-00:00"\
+### gridEngineArrayMaxJobs=2 \
 
 # Finish
 echo "Finish HiCanu"
