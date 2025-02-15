da861cd539315a77902bdcaf3d969874754e9504
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-15 15:08:14 -0500

    Update concatVCF.sh

--- concatVCF.sh
+++ concatVCF.sh
@@ -19,104 +19,104 @@ module load htslib bcftools ### <- different module
 
  bgzip \
  -i -@ 10 \
- /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf \
+ /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf \
 
 
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
 
  bcftools index \
-  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz \
 
 
 bcftools concat \
 --threads 10 \
--o  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.concat.renamed.vcf.gz \
+-o  /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.concat.renamed.vcf.gz \
 -O z \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
-/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz
