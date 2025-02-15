7cbe1be581eb0a5ba3136792ede740fb89b4fcea
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-15 16:17:11 -0500

    Update RemoveRepetitiveRegions.sh

--- RemoveRepetitiveRegions.sh
+++ RemoveRepetitiveRegions.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1 # on one node
 #SBATCH -t 0-2:00  ### 1 hours
 #SBATCH --mem 24G
-#SBATCH -o /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/Bams/vcf/2022seq.concat.Removereps.vcf.gz
+#SBATCH -o /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.concat.Removereps.renamed.vcf.gz
 #SBATCH -e /scratch/rjp5nc/outputerrors/download_%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
@@ -15,9 +15,8 @@
 ### scancel
 ### cat
 
-module load gcc/9.2.0 bedtools/2.29.2
+module load gcc/12.4.0 bedtools/2.30.0 htslib
 
-bedtools subtract \
--header \
+bedtools subtract -header \
 -a /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.concat.renamed.vcf.gz \
--b /project/berglandlab/daphnia_ref/RMoutHiCGMgoodscaff.keep.bed 
+-b /project/berglandlab/daphnia_ref/RMoutHiCGMgoodscaff.keep.bed
