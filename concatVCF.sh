73ddc6b8b7f4510135da2f3e388f1ac5479d1a10
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-15 15:06:13 -0500

    Update concatVCF.sh

--- concatVCF.sh
+++ concatVCF.sh
@@ -3,7 +3,7 @@
 #SBATCH -J concatVCF # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-2:00  ### 1 hours
+#SBATCH -t 0-4:00  ### 1 hours
 #SBATCH --mem 24G
 #SBATCH -o /scratch/rjp5nc/download_%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/download_%A_%a.err # Standard error
@@ -19,104 +19,104 @@ module load htslib bcftools ### <- different module
 
  bgzip \
  -i -@ 10 \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1863_HRSCAF_2081.vcf \
+ /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1931_HRSCAF_2197.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2158_HRSCAF_2565.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2217_HRSCAF_2652.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2373_HRSCAF_2879.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_6786_HRSCAF_7541.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_7757_HRSCAF_8726.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9197_HRSCAF_10753.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9198_HRSCAF_10754.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9199_HRSCAF_10755.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9200_HRSCAF_10757.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf \
 
  bgzip \
  -i -@ 10 \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9201_HRSCAF_10758.vcf \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf \
 
 
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1863_HRSCAF_2081.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1931_HRSCAF_2197.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2158_HRSCAF_2565.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2217_HRSCAF_2652.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2373_HRSCAF_2879.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_6786_HRSCAF_7541.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_7757_HRSCAF_8726.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9197_HRSCAF_10753.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9198_HRSCAF_10754.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9199_HRSCAF_10755.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9200_HRSCAF_10757.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
 
  bcftools index \
-  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9201_HRSCAF_10758.vcf.gz \
+  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz \
 
 
 bcftools concat \
 --threads 10 \
--o  /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.concat.vcf.gz \
+-o  /scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.concat.renamed.vcf.gz \
 -O z \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1863_HRSCAF_2081.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_1931_HRSCAF_2197.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2217_HRSCAF_2652.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2158_HRSCAF_2565.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_2373_HRSCAF_2879.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_6786_HRSCAF_7541.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_7757_HRSCAF_8726.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9197_HRSCAF_10753.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9198_HRSCAF_10754.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9199_HRSCAF_10755.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9200_HRSCAF_10757.vcf.gz \
- /project/berglandlab/Robert/UKSequencing2022_2024/usftp21.novogene.com/01.RawData/vcf/2022seq.Scaffold_9201_HRSCAF_10758.vcf.gz
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1863_HRSCAF_2081.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_1931_HRSCAF_2197.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2217_HRSCAF_2652.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2158_HRSCAF_2565.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_2373_HRSCAF_2879.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_6786_HRSCAF_7541.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_7757_HRSCAF_8726.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9197_HRSCAF_10753.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9198_HRSCAF_10754.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9199_HRSCAF_10755.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9200_HRSCAF_10757.renamed.vcf.gz \
+/scratch/rjp5nc/UK2022_2024/allshortreads/2022seq.Scaffold_9201_HRSCAF_10758.renamed.vcf.gz
