622ee8a9503069fd8526031bc66fba47e698056e
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-20 22:35:23 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,8 +3,8 @@
 #SBATCH -J download2l # A single job name for the array
 #SBATCH --ntasks-per-node=10 # ten core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-24:00  ### 24 hours
-#SBATCH --mem 60G
+#SBATCH -t 0-36:00  ### 24 hours
+#SBATCH --mem 10G
 #SBATCH -o /scratch/rjp5nc/basicstats.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/basicstats.%A_%a.err # Standard error
 #SBATCH -p standard
@@ -16,6 +16,6 @@
 
 module load lftp
 
-cd /project/berglandlab/Robert/UKSequencing2022_2024/2lanes
+cd /scratch/rjp5nc/2ldup
 
 lftp -e "set sftp:auto-confirm yes; open sftp://X202SC25016886-Z01-F001:dxk06fch@usftp23.novogene.com:3022; mirror --verbose --use-pget-n=8 -c"
