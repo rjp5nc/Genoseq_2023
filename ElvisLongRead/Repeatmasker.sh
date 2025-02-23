83b9e79b6460468e2ce38097252070733e23b167
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-02-22 21:48:20 -0500

    up

--- ElvisLongRead/Repeatmasker.sh
+++ ElvisLongRead/Repeatmasker.sh
@@ -1,10 +1,33 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J run_modeler # A single job name for the array
+#SBATCH --ntasks-per-node=20 # multi core
+#SBATCH -N 1 # on one node
+#SBATCH -t 1-00:00 # 1 days
+#SBATCH --mem 100G
+#SBATCH -o /scratch/rjp5nc/outputerrors/m%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/outputerrors/m%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
 
 
-conda create -n repeatmodeler_env -c bioconda -c conda-forge repeatmodeler perl=5.22.0
+
+
+
+#conda create -n quast_env python=3.9
+#conda activate quast_env
+#conda install -c bioconda quast
+
+#quast.py /scratch/rjp5nc/assemblecontigs/dap.contigs.fasta -o /scratch/rjp5nc/assemblecontigs
+
+
+#conda create -n repeatmodeler_env -c bioconda -c conda-forge repeatmodeler perl=5.22.0
 conda activate repeatmodeler_new
+#seqtk seq dap.contigs.fasta > cleaned_dap.contigs.fasta
+#makeblastdb -in cleaned_dap.contigs.fasta -out my_db -dbtype nucl -title my_db -parse_seqids
+#BuildDatabase -name my_db dap.contigs.fasta
 
-seqtk seq dap.contigs.fasta > cleaned_dap.contigs.fasta
+wd="/scratch/rjp5nc/removedups"
+cd ${wd}
 
-makeblastdb -in cleaned_dap.contigs.fasta -out my_db -dbtype nucl -title my_db -parse_seqids
-BuildDatabase -name my_db dap.contigs.fasta
-RepeatModeler -database my_db -pa 4
+RepeatModeler -database my_db -pa 20
