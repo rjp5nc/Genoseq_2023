9538a5030c700f5bc08868bfb38c30003ff55834
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-03-07 19:03:56 -0500

    Update runkraken.sh

--- ElvisLongRead/runkraken.sh
+++ ElvisLongRead/runkraken.sh
@@ -13,10 +13,12 @@
 
 module load kraken2
 
+KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
+
 echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
 source ~/.bashrc
 
-kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 30 --report /scratch/rjp5nc/krakenDB/report.txt --output /scratch/rjp5nc/krakenDB/kraken_output.txt /scratch/rjp5nc/HMW/HMWDNAElvis3/hifiasm_out/assembly.hap2.p_ctg.fa
+kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 30 --report /scratch/rjp5nc/krakenDB/report.txt --classified-out /scratch/rjp5nc/krakenDB/kraken_classified_output.txt --output /scratch/rjp5nc/krakenDB/kraken_output.txt /scratch/rjp5nc/HMW/HMWDNAElvis3/hifiasm_out/assembly.hap2.p_ctg.fa
 
 
 #kraken2-build --standard --db /scratch/rjp5nc/krakenDB/test2
