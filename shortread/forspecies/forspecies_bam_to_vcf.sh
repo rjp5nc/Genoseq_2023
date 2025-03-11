0581381e2d76d81cd368578c4d0e458f828b7416
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-03-10 22:15:47 -0400

    up

--- shortread/forspecies/forspecies_bam_to_vcf.sh
+++ shortread/forspecies/forspecies_bam_to_vcf.sh
@@ -14,18 +14,18 @@
 module load samtools varscan bcftools
 
 # Store BAM file list in a variable to maintain order
-bam_list=$(ls /scratch/rjp5nc/UK2022_2024/allshortreads/mitosortedbams/*.sorted.bam | sort)
+bam_list=$(ls /scratch/rjp5nc/UK2022_2024/allshortreads/alignedbamsnew/*.sorted_allreads.bam | sort)
 
 # Generate VCF with VarScan
 samtools mpileup \
-    --fasta-ref /scratch/rjp5nc/UK2022_2024/allshortreads/allcoi_updated22.fa \
+    --fasta-ref /scratch/rjp5nc/UK2022_2024/allshortreads/mtdna_D8_119.fa \
     $bam_list | \
 
 java -jar $EBROOTVARSCAN/VarScan.v2.4.4.jar mpileup2snp \
     /dev/stdin \
     --min-coverage 4 \
     --min-var-freq 0.001 \
-    --output-vcf > /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF.vcf
+    --output-vcf > /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF2.vcf
 
 # Extract sample names in correct order
 echo "$bam_list" | sed 's#.*/##; s/.bam//' > sample_names.txt
@@ -33,5 +33,5 @@ echo "$bam_list" | sed 's#.*/##; s/.bam//' > sample_names.txt
 # Reheader the VCF with the correct sample order
 bcftools reheader \
     --samples sample_names.txt \
-    /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF.vcf > \
-    /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF_renamed.vcf
+    /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF2.vcf > \
+    /scratch/rjp5nc/UK2022_2024/allshortreads/mitoVCF_renamed2.vcf
