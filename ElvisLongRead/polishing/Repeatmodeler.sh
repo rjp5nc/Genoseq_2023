ccc194a20624d99ec6d4d6172572283880a64891
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-03-23 22:07:06 -0400

    Update Repeatmodeler.sh

--- ElvisLongRead/polishing/Repeatmodeler.sh
+++ ElvisLongRead/polishing/Repeatmodeler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J run_modeler # A single job name for the array
-#SBATCH --ntasks-per-node=20 # multi core
+#SBATCH --ntasks-per-node=15 # multi core
 #SBATCH -N 1 # on one node
 #SBATCH -t 5-00:00 # 1 days
 #SBATCH --mem 100G
@@ -26,6 +26,9 @@ cd ${wd}
 
 #seqtk seq assembly.hap2_onlydaps.fasta > cleaned_dap.contigs.fasta
 #makeblastdb -in cleaned_dap.contigs.fasta -out my_db -dbtype nucl -title my_db -parse_seqids
-#BuildDatabase -name my_db dap.contigs.fasta
+#BuildDatabase -name my_db assembly.hap2_onlydaps.fasta
 
-RepeatModeler -database my_db -pa 20
+#RepeatModeler -database my_db -pa 15
+
+
+RepeatModeler -database my_db -pa 15 -recoverDir RM_776028.SunMar231202032025
