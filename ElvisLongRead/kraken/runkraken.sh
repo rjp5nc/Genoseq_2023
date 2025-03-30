634476f38f6087ad22fc75988c70be1d1b3c3086
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-03-30 16:50:02 -0400

    Update runkraken.sh

--- ElvisLongRead/kraken/runkraken.sh
+++ ElvisLongRead/kraken/runkraken.sh
@@ -13,10 +13,11 @@
 
 module load kraken2
 
-KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
+export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
+#KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
 
-echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
-source ~/.bashrc
+#echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
+#source ~/.bashrc
 
 kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 30 --report /scratch/rjp5nc/krakenDB/US_obtusa/report.txt --classified-out /scratch/rjp5nc/krakenDB/US_obtusa/kraken_classified_output.txt --output /scratch/rjp5nc/krakenDB/US_obtusa/kraken_output.txt --use-names /scratch/rjp5nc/Reference_genomes/orig_ref/JAACYE01.1.fasta
 
