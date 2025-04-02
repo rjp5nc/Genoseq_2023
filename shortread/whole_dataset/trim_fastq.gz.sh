76dda0c9f220599b1baa67d31974f9e732b7e5fc
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-02 19:03:02 -0400

    Update trim_fastq.gz.sh

--- shortread/whole_dataset/trim_fastq.gz.sh
+++ shortread/whole_dataset/trim_fastq.gz.sh
@@ -3,7 +3,7 @@
 #SBATCH -J trimSRA # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 2-10:00 # 10 hours
+#SBATCH -t 0-10:00 # 10 hours
 #SBATCH --mem 100G
 #SBATCH -o /scratch/rjp5nc/outputerrors/down.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/outputerrors/down.%A_%a.err # Standard error
@@ -13,34 +13,40 @@
 
 #mv /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SR* /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR/
 
-PARENT_DIR="/scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR/"  # Change this to your actual path
-cd "$PARENT_DIR" || exit
+PARENT_DIR="/scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR"
 
-for folder in */; do
-    folder_name="${folder%/}"  # Remove trailing slash from folder name
-    echo "Entering folder: $folder_name"
-    cd "$folder_name" || continue  # Enter folder
+# Get list of all folders
+FOLDERS=($(ls -d ${PARENT_DIR}/*/))  # Array of folder paths
 
- if ls *.fastq.gz 1> /dev/null 2>&1; then
-        echo "trimming files in $folder_name..."
-trimmomatic PE -threads 10 \
-${folder_name}_1.fastq.gz \
-${folder_name}_2.fastq.gz \
-${folder_name}_1.P.trimm.fastq.gz \
-${folder_name}_1.U.trimm.fastq.gz \
-${folder_name}_2.P.trimm.fastq.gz \
-${folder_name}_2.U.trimm.fastq.gz \
-ILLUMINACLIP:/home/rjp5nc/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
+# Select the folder based on the job array index
+FOLDER="${FOLDERS[$SLURM_ARRAY_TASK_ID - 1]}"
 
-# Merge overlapping reads
-/home/rjp5nc/miniconda3/bin/pear \
--f ${folder_name}_1.P.trimm.fastq.gz \
--r ${folder_name}_2.P.trimm.fastq.gz \
--o ${folder_name} \
--j 10
-    else
-        echo "Warning: No fastq.gz in $folder_name"
-    fi
-    cd ..
-done
+# Extract folder name
+FOLDER_NAME=$(basename "$FOLDER")
 
+# Change to the working directory
+cd "$FOLDER" || exit 1
+
+# Check if fastq.gz files exist
+if ls *.fastq.gz 1> /dev/null 2>&1; then
+    echo "Processing $FOLDER_NAME"
+
+    # Run Trimmomatic
+    trimmomatic PE -threads 10 \
+        ${FOLDER_NAME}_1.fastq.gz \
+        ${FOLDER_NAME}_2.fastq.gz \
+        ${FOLDER_NAME}_1.P.trimm.fastq.gz \
+        ${FOLDER_NAME}_1.U.trimm.fastq.gz \
+        ${FOLDER_NAME}_2.P.trimm.fastq.gz \
+        ${FOLDER_NAME}_2.U.trimm.fastq.gz \
+        ILLUMINACLIP:/home/rjp5nc/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
+
+    # Run PEAR to merge overlapping reads
+    /home/rjp5nc/miniconda3/bin/pear \
+        -f ${FOLDER_NAME}_1.P.trimm.fastq.gz \
+        -r ${FOLDER_NAME}_2.P.trimm.fastq.gz \
+        -o ${FOLDER_NAME} \
+        -j 10
+else
+    echo "Warning: No fastq.gz files in $FOLDER_NAME"
+fi
