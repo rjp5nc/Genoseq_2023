d538f7e6564bf710deacf67b2093da8c5965d37a
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-03 14:29:55 -0400

    Update trim_fastq.gz.sh

--- shortread/whole_dataset/trim_fastq.gz.sh
+++ shortread/whole_dataset/trim_fastq.gz.sh
@@ -9,11 +9,11 @@
 #SBATCH -e /scratch/rjp5nc/outputerrors/down.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
-#SBATCH --array=1-79%10   # Adjust the range based on the number of folders
+#SBATCH --array=1-576%20   # Adjust the range based on the number of folders
 
 #mv /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SR* /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR/
 
-PARENT_DIR="/scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR"
+PARENT_DIR="/scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/newseq"
 
 # Get list of all folders
 FOLDERS=($(ls -d ${PARENT_DIR}/*/))  # Array of folder paths
@@ -28,25 +28,25 @@ FOLDER_NAME=$(basename "$FOLDER")
 cd "$FOLDER" || exit 1
 
 # Check if fastq.gz files exist
-if ls *.fastq.gz 1> /dev/null 2>&1; then
+if ls *.fq.gz 1> /dev/null 2>&1; then
     echo "Processing $FOLDER_NAME"
 
     # Run Trimmomatic
-    trimmomatic PE -threads 10 \
-        ${FOLDER_NAME}_1.fastq.gz \
-        ${FOLDER_NAME}_2.fastq.gz \
-        ${FOLDER_NAME}_1.P.trimm.fastq.gz \
-        ${FOLDER_NAME}_1.U.trimm.fastq.gz \
-        ${FOLDER_NAME}_2.P.trimm.fastq.gz \
-        ${FOLDER_NAME}_2.U.trimm.fastq.gz \
-        ILLUMINACLIP:/home/rjp5nc/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
+ #   trimmomatic PE -threads 10 \
+ #       ${FOLDER_NAME}_trimmedmerged1.fq.gz \
+ #       ${FOLDER_NAME}_trimmedmerged1.fq.gz \
+ #       ${FOLDER_NAME}_1.P.trimm.fastq.gz \
+ #       ${FOLDER_NAME}_1.U.trimm.fastq.gz \
+ #       ${FOLDER_NAME}_2.P.trimm.fastq.gz \
+ #       ${FOLDER_NAME}_2.U.trimm.fastq.gz \
+ #       ILLUMINACLIP:/home/rjp5nc/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
 
     # Run PEAR to merge overlapping reads
     /home/rjp5nc/miniconda3/bin/pear \
-        -f ${FOLDER_NAME}_1.P.trimm.fastq.gz \
-        -r ${FOLDER_NAME}_2.P.trimm.fastq.gz \
+        -f ${FOLDER_NAME}_trimmedmerged1.fq.gz \
+        -r ${FOLDER_NAME}_trimmedmerged2.fq.gz \
         -o ${FOLDER_NAME} \
         -j 10
 else
-    echo "Warning: No fastq.gz files in $FOLDER_NAME"
+    echo "Warning: No fq.gz files in $FOLDER_NAME"
 fi
