3edd633ee31ff381bfef612dc873e1d81f89084e
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-16 14:08:57 -0400

    up

---
+++ shortread/liftover/lastz.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#SBATCH -J runFASTQC # A single job name for the array
+#SBATCH --ntasks-per-node=40 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 4:00:00 ### 15 seconds
+#SBATCH --mem 120G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/nFlo_1.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/nFlo_1.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+# sbatch /home/rjp5nc/Genoseq_2023/shortread/liftover/lastz.sh
+# sacct -j 761713
+
+module load nextflow/23.04.1
+module load apptainer/1.3.4
+
+### ssdf
+### test container
+  #nextflow run evotools/nf-LO -profile test,singularity -with-singularity ${PWD}/nflo.sif
+
+#mkdir -p /scratch/rjp5nc/lastz/us_pulex
+
+
+  nextflow run evotools/nf-LO \
+  --source /scratch/rjp5nc/Reference_genomes/post_kraken/us_pulex_ref_kap4.fa \
+  --target /scratch/rjp5nc/Reference_genomes/post_kraken/eu_pulex_totalHiCwithallbestgapclosed.clean.fa \
+  --distance medium \
+  --aligner lastz \
+  --outdir /scratch/rjp5nc/lastz/us_pulex \
+  -profile singularity \
+  --max_memory '60.GB' \
+  --max_cpus 40 \
+  -with-singularity /scratch/rjp5nc/nflo/nflo.sif
