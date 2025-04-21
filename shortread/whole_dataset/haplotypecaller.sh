5cec90dbc6e183482cf00a632503a4bd2360ea94
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-21 15:36:54 -0400

    up

---
+++ shortread/whole_dataset/haplotypecaller.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J gatk_chrom # A single job name for the array
+#SBATCH --ntasks-per-node=1 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 3-00:00:00 # 3 days
+#SBATCH --mem 25G
+#SBATCH -o /scratch/csm6hg/daphnia_phylo/err/gatk.chrom.%A_%a.out # Standard output
+#SBATCH -e /scratch/csm6hg/daphnia_phylo/err/gatk.chrom.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --array=1-100%40
+
+
+# grep '^>' your_file.fasta | sed 's/^>//'
+# grep '^>' assembly.hap2_onlydaps.fasta | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/assembly.hap2_onlydaps_chr.txt
+# grep '^>' Daphnia_ambigua_Q001_genome.fa | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/ambigua_chr.txt
+# grep '^>' eu_pulex_totalHiCwithallbestgapclosed.clean.fa | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/eu_pulex_chr.txt
+# grep '^>' US_obtusa_onlydaps.fa | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/us_obtusa_chr.txt
+# grep '^>' us_pulex_ref_kap4.fa | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/us_pulex_chr.txt
+# grep '^>' eu_pulex_totalHiCwithallbestgapclosed.fa | sed 's/^>//' > /scratch/rjp5nc/UK2022_2024/eu_pulex_all_chr.txt
+
+#gatk CreateSequenceDictionary -R Daphnia_ambigua_Q001_genome.fa
+#gatk CreateSequenceDictionary -R totalHiCwithallbestgapclosed.fa
+#gatk CreateSequenceDictionary -R assembly.hap2_onlydaps.fasta
+#gatk CreateSequenceDictionary -R us_pulex_ref_kap4.fa
+
+#grep "^>h2tg000002l" assembly.hap2_onlydaps.fasta
+
+# Load modules
+module load gatk/4.6.0.0
+module load tabix/0.2.6
+
+# Parameters     find /project/berglandlab/connor -type f -name "SRA_paramList_1"
+
+
+#parameterFile=/scratch/rjp5nc/UK2022_2024/robert_paramfile.txt
+parameterFile=/scratch/rjp5nc/UK2022_2024/param100.txt
+wd="/scratch/rjp5nc/UK2022_2024/daphnia_phylo"
+
+#dos2unix "$parameterFile"
+
+SLURM_ARRAY_TASK_ID=2
+
+# Extract sample name
+id=$(awk -F',' -v task_id="$SLURM_ARRAY_TASK_ID" 'NR == task_id + 1 {print $7}' "$parameterFile")
+samp=$(awk -F',' -v task_id="$SLURM_ARRAY_TASK_ID" 'NR == task_id + 1 {print $2}' "$parameterFile")
+chrom=$(awk -F',' -v task_id="$SLURM_ARRAY_TASK_ID" 'NR == task_id + 1 {print $6}' "$parameterFile")
+ref=$(awk -F',' -v task_id="$SLURM_ARRAY_TASK_ID" 'NR == task_id + 1 {print $5}' "$parameterFile")
+
+
+echo "Haplotype calling -" "Sample:" $SLURM_ARRAY_TASK_ID
+echo "Chromosome:" ${chrom}
+
+# Create folder for chromosome
+if [[ -d "${wd}/gvcf/${chrom}" ]]
+then
+	echo "Working chromosome folder exist"
+	echo "lets move on"
+	date
+else
+	echo "folder doesnt exist. lets fix that"
+	mkdir ${wd}/gvcf/${chrom}
+	date
+fi
+
+
+#gatk CreateSequenceDictionary -R "$ref"
+#samtools faidx "$ref"
+
+
+# Haplotype Calling
+gatk HaplotypeCaller \
+-R $ref \
+-I /scratch/rjp5nc/UK2022_2024/final_bam_rg/${samp}finalmap_RG.bam \
+-O ${wd}/gvcf/${chrom}/${samp}.${chrom}.${id}.g.vcf \
+-L ${wd}/bed/${chrom}.bed \
+-ERC GVCF
+
+
+
+# Bgzip
+module load gcc/14.2.0 htslib/1.17
+
+# Compress and index with Tabix
+bgzip ${wd}/gvcf/${chrom}/${samp}.${chrom}.${id}.g.vcf
+tabix -p vcf ${wd}/gvcf/${chrom}/${samp}.${chrom}.${id}.g.vcf.gz
+
+# Finish
+echo "Complete -" "Sample:" $SLURM_ARRAY_TASK_ID
