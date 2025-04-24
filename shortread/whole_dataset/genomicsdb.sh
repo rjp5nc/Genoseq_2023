fe443c2b1c31c25e8b847ecb7a4fad48e14b3536
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-24 16:26:23 -0400

    Update genomicsdb.sh

--- shortread/whole_dataset/genomicsdb.sh
+++ shortread/whole_dataset/genomicsdb.sh
@@ -14,35 +14,28 @@
 # This will be done using a per chromosome approach
 
 
+#for f in *.g.vcf.gz; do
+#    clean_name=$(echo "$f" | tr -d '\r')
+#    echo "$clean_name"
+#done
 
-
-
-find /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf/euobtusa_chr -type f -name "*.g.vcf.gz" | while read file; do
-    printf "%s\t%s\n" "$(basename "$file")" "$file"
-done > 
-
-
-find /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf -type f -name "*.g.vcf.gz" | \
-awk '{ file=$0; split(file, a, "/"); printf "%-40s %s\n", a[length(a)], file }' > gvcf_euobtusa_list.txt
-
-find /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf -type f -name "*.g.vcf.gz" | \
-while IFS= read -r filepath; do
-    filename=$(basename "$filepath")
-    echo -e "$filename\t$filepath"
-done > gvcf_file_list.txt
+#find $(pwd) -type f -name "*.g.vcf.gz" | while read f; do
+#    clean_name=$(echo "$f" | tr -d '\r')  # Remove any \r characters if present
+#    echo -e "$(basename "$clean_name")\t$clean_name" >> cleaned_file_list.txt
+#done
 
 
 # Load modules
-module load gatk/4.1.6.0
+module load gatk/4.6.0.0
 
 # Name of pipeline
 PIPELINE="GenomicsDBImport"
 
 # Working folder is core folder where this pipeline is being run.
-WORKING_FOLDER="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/DBI"
+WORKING_FOLDER="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/DBI_euobtusa"
 
 # Chromosomes to analyze
-intervals=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/interval_DBI_paramList
+intervals=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/interval_DBI_paramList_euobtusa.txt
 
 # Parameters
 JAVAMEM=80G
@@ -78,7 +71,7 @@ gatk --java-options "-Xmx${JAVAMEM}" GenomicsDBImport \
 --genomicsdb-workspace-path $WORKING_FOLDER/Daphnia_DBI_${i}_${start}_${stop} \
 --tmp-dir $WORKING_FOLDER/TEMP_Daphnia_DBI_${i}_${start}_${stop} \
 --batch-size 50 \
---sample-name-map /scratch/csm6hg/daphnia_phylo/data/interval.total.test.txt \
+--sample-name-map /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf/euobtusa_chr/euobtusa_gvcflist.txt \
 --reader-threads $CPU \
 -L ${i}:${start}-${stop}
 
