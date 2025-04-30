54f9abcdc4739c5ed945e33161a034d3bdbe6e8a
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-04-29 23:27:39 -0400

    up

---
+++ shortread/liftover/crossmap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#SBATCH -J crossmap # A single job name for the array
+#SBATCH --ntasks-per-node=1 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 12:00:00 
+#SBATCH --mem 200G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/crossmap.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/crossmap.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --mail-type=END               # Send email at job completion
+#SBATCH --mail-user=myu5wx@virginia.edu    # Email address for notifications
+
+#Load necessary modules (if needed)
+module load gcc/11.4.0
+module load openmpi/4.1.4
+module load python/3.11.4
+
+#pip install --user .
+
+species=us_obtusa
+
+/home/rjp5nc/.local/bin/CrossMap bed --chromid a \
+/scratch/rjp5nc/lastz/$species/chainnet/liftover.chain \
+/scratch/rjp5nc/Reference_genomes/eu_pulex_totalHiCwithallbestgapclosed.clean.allbases.bed \
+/scratch/rjp5nc/liftover/usobtusa_to_eupulex.bed
