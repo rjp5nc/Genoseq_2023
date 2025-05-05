95954c6096b044ae7fed4aef7989426ca7d27f02
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-05-05 15:49:55 -0400

    up

--- shortread/whole_dataset/renamegvcf.sh
+++ shortread/whole_dataset/renamegvcf.sh
@@ -1,22 +1,25 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J rename # A single job name for the array
+#SBATCH --ntasks-per-node=10 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 12:00:00 # 8 hours
+#SBATCH --mem 10G
+#SBATCH -o /scratch/rjp5nc/err/gatk.chrom.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/err/gatk.chrom.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
 
-cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf/euobtusa_chr/h2tg000002l_2
+# This script will merge gVCFs into a unified database for genotype calling.
+# This will be done using a per chromosome approach
 
-#!/bin/bash
+target_dir="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf"
 
-# Fix all .g.vcf.gz files under current directory and subdirectories
-#sed to remove the path  : /scratch/rjp5nc/UK2022_2024/mapped_bam/
-
-find . -name "*.g.vcf.gz" | while read gzfile; do
-    echo "Processing $gzfile"
-    
-    # Uncompress to temporary file
-    gunzip -c file.g.vcf.gz | sed 's/\r$//' > file.g.vcf
-    bgzip -f file.g.vcf
-       
-    # Remove paths and suffixes from within the file content
-    sed -i 's|/scratch/rjp5nc/UK2022_2024/mapped_bam/||g; s|_finalmap.bam||g' "${gzfile%.gz}"
-    
-    gatk IndexFeatureFile -I file.g.vcf.gz
-    # Recompress back
-    bgzip -f "${gzfile%.gz}"
+# Find and rename files with carriage returns in their names
+find "$target_dir" -type f | while IFS= read -r file; do
+  if [[ "$file" == *$'\r'* ]]; then
+    clean_file="$(echo "$file" | tr -d '\r')"
+    echo "Renaming: $file -> $clean_file"
+    mv "$file" "$clean_file"
+  fi
 done
