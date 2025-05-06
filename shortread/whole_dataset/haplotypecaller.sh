090f6c47b338cedcd00398709494430dba885966
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-05-06 13:48:30 -0400

    Update haplotypecaller.sh

--- shortread/whole_dataset/haplotypecaller.sh
+++ shortread/whole_dataset/haplotypecaller.sh
@@ -58,9 +58,9 @@ module load tabix/0.2.6
 #sed -n '89992,99990p' /scratch/rjp5nc/UK2022_2024/robert_paramfile.txt > /scratch/rjp5nc/UK2022_2024/param10.txt
 #sed -n '99991,101136p' /scratch/rjp5nc/UK2022_2024/robert_paramfile.txt > /scratch/rjp5nc/UK2022_2024/param11.txt
 
-#finished Param1
+#finished param10000_19998
 
-parameterFile=/scratch/rjp5nc/UK2022_2024/param1_9999.txt
+parameterFile=/scratch/rjp5nc/UK2022_2024/param10000_19998.txt
 wd="/scratch/rjp5nc/UK2022_2024/daphnia_phylo"
 
 #dos2unix "$parameterFile"
