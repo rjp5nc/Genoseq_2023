2c0077e1b8531d2e56143a6e419c2671351452bb
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-05-06 21:07:12 -0400

    Update haplotypecaller.sh

--- shortread/whole_dataset/haplotypecaller.sh
+++ shortread/whole_dataset/haplotypecaller.sh
@@ -3,7 +3,7 @@
 #SBATCH -J gatk_chrom # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-12:00:00 # 8 hours
+#SBATCH -t 0-6:00:00 # 8 hours
 #SBATCH --mem 25G
 #SBATCH -o /scratch/rjp5nc/err/gatk.chrom.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/err/gatk.chrom.%A_%a.err # Standard error
