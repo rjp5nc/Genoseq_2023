8be6fc7e05877c87b3c7805019cddf750d31a491
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-05-28 10:17:32 -0400

    up

--- ElvisLongRead/polishing/Repeatmasker.sh
+++ ElvisLongRead/polishing/Repeatmasker.sh
@@ -1,6 +1,28 @@
-
-
-
-
-
+#!/usr/bin/env bash
 #
+#SBATCH -J run_masker # A single job name for the array
+#SBATCH --ntasks-per-node=10 # multi core
+#SBATCH -N 1 # on one node
+#SBATCH -t 1-00:00 # 1 days
+#SBATCH --mem 150G
+#SBATCH -o /scratch/rjp5nc/err/m%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/err/m%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+
+conda activate repeatmodeler_new
+
+path=/scratch/rjp5nc/removedups/finalfiles/usobtusa
+
+mkdir -p $path
+
+cd $path 
+
+ref=/scratch/rjp5nc/Reference_genomes/post_kraken/US_obtusa_onlydaps.fa
+classified=/scratch/rjp5nc/removedups/us_dobtusa/RM_653473.SunMar301757312025/consensi.fa.classified
+
+export REPEATMASKER_NO_GENLIB=1
+export REPEATMASKER_NO_TAXONOMY=1
+
+RepeatMasker -lib $classified -dir . $ref
