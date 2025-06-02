0d1a5d26c2f6a4717bf6336661ef3588daf7e7ad
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-06-02 15:00:46 -0400

    up

---
+++ ElvisLongRead/polishing/bgzip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J bgzip # A single job name for the array
+#SBATCH --ntasks-per-node=10 # multi core
+#SBATCH -N 1 # on one node
+#SBATCH -t 1-00:00 # 1 days
+#SBATCH --mem 60G
+#SBATCH -o /scratch/rjp5nc/err/m%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/err/m%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+module load bcftools
+module load samtools
+bgzip /scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/trimmed10bp_masked_usobtusa.vcf.gz
---
+++ ElvisLongRead/polishing/repeatmasker_docker.sh
@@ -0,0 +1,35 @@
+# ijob -A berglandlab -c4 -p standard --mem=10G
+
+### RepeatMakser image
+### https://github.com/Dfam-consortium/TETools
+
+module load apptainer
+
+### install docker
+singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest
+curl -sSLO https://github.com/Dfam-consortium/TETools/raw/master/dfam-tetools.sh
+chmod +x dfam-tetools.sh
+/scratch/rjp5nc/dfamtools/dfam-tetools.sh -- RepeatModeler -h
+/scratch/rjp5nc/dfamtools/dfam-tetools.sh -- RepeatMasker -h
+
+### make library
+
+
+
+
+
+ref=/scratch/rjp5nc/Reference_genomes/post_kraken/US_obtusa_onlydaps.fa
+classified=/scratch/rjp5nc/removedups/us_dobtusa/RM_653473.SunMar301757312025/consensi.fa.classified
+
+
+### try running RepeatMasker
+~/dfam-tetools.sh -- RepeatMasker -h
+/scratch/rjp5nc/dfamtools/dfam-tetools.sh \
+-- RepeatMasker \
+-e rmblast \
+-pa 4 \
+-qq \
+-lib $classified \
+-dir /scratch/rjp5nc/removedups/eu_dobtusa/ \
+-html \
+$ref
