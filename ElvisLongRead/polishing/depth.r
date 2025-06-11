59e8d31c82dc78af87a78ad4fdb6eca90d5672ef
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-06-11 07:09:44 -0400

    Update depth.r

--- ElvisLongRead/polishing/depth.r
+++ ElvisLongRead/polishing/depth.r
@@ -9,7 +9,7 @@ library(stringr)
 library(ggplot2)
 
 
-vcf <- read.vcfR("/scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/trimmed10bp_masked_usobtusa.vcf.gz")
+vcf <- read.vcfR("/scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/lifted_vcf/lifted_all.vcf.gz")
 
 # Extract DP (Depth) values from the VCF file
 dp_matrix <- extract.gt(vcf, element = "DP", as.numeric = TRUE)
@@ -17,5 +17,5 @@ dp_matrix <- extract.gt(vcf, element = "DP", as.numeric = TRUE)
 # Calculate average depth per variant (row-wise mean)
 avg_depth_per_sample <- colMeans(dp_matrix, na.rm = TRUE)
 
-write.csv(avg_depth_df, "/scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/usobtusa_dp.csv",
+write.csv(avg_depth_df, "/scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/lifted_all_depth.csv",
           row.names = FALSE)
