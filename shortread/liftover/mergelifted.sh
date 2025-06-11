fe75dd2f3b9524a9b75b49f27b18783826c5a57b
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-06-10 22:35:36 -0400

    Update mergelifted.sh

--- shortread/liftover/mergelifted.sh
+++ shortread/liftover/mergelifted.sh
@@ -11,25 +11,8 @@
 #SBATCH --account berglandlab
 
 module load gatk/4.6.0.0  # adjust version as needed
-
-species=uspulex
-speciescrossmap=us_pulex
-vcffile=trimmed10bp_masked_${species}.vcf.gz
-sourcegenome=totalHiCwithallbestgapclosed.fa
+module load bcftools 
 
 cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/trimmed_10bp_repeatmasked_vcf/lifted_vcf
 
-# Define species name
-input_dir="."  # or wherever your VCFs are
-output_vcf="lifted_all.vcf.gz"
-
-# Create a list of input files
-inputs=$(ls ${input_dir}/*.vcf.gz | sed 's/^/--INPUT /' | tr '\n' ' ')
-
-# Merge
-gatk MergeVcfs \
-  ${inputs} \
-  --OUTPUT ${output_vcf}
-
-# Index
-gatk IndexFeatureFile -I ${output_vcf}
+bcftools merge -Oz -o lifted_all.vcf.gz *.vcf.gz
