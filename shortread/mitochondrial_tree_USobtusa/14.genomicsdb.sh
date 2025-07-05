8e1a0258af2ea89348b513ec635baf4e276f2a4b
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-04 23:41:09 -0400

    Update 14.genomicsdb.sh

--- shortread/mitochondrial_tree_USobtusa/14.genomicsdb.sh
+++ shortread/mitochondrial_tree_USobtusa/14.genomicsdb.sh
@@ -32,10 +32,17 @@ PIPELINE="GenomicsDBImport"
 #folder=kap4Dpulex_mito
 folder=usdobtusa_mito
 
+# Parameters
+
+# Move to working directory
+cd $WORKING_FOLDER
+
+# Chromosome
+
 WORKING_FOLDER="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/DBI_mitos/${folder}"
 
 mkdir -p "${WORKING_FOLDER}" 
-
+mkdir -p "$WORKING_FOLDER/temp"
 # Parameters
 JAVAMEM=40G
 CPU=10
@@ -58,11 +65,26 @@ cd $WORKING_FOLDER
 #zcat /scratch/rjp5nc/UK2022_2024/daphnia_phylo/gvcf/euobtusa_chr/h2tg000002l/Gilmer5_H9.h2tg000002l.1018082.g.vcf.gz | head -n 500
 
 
+sample_list="/scratch/rjp5nc/UK2022_2024/sample_map_fixed.txt"
+
+# Read line and split into two variables
+read -r sample_id gvcf_path < <(awk -F'\t' -v line=${SLURM_ARRAY_TASK_ID} 'NR==line' ${sample_list})
+
+echo "Sample ID: $sample_id"
+echo "GVCF Path: $gvcf_path"
+
+interval=$(awk '{printf "%s:%d-%d", $1, $2+1, $3}' /scratch/rjp5nc/Reference_genomes/mito_reference/${folder}.bed)
+echo "$interval"
+
+mkdir -p "${WORKING_FOLDER}/TEMP_Daphnia_DBI_${species}_${sample_id}"
+
 gatk --java-options "-Xmx${JAVAMEM}" GenomicsDBImport \
-  --genomicsdb-workspace-path $WORKING_FOLDER \
-  -L /scratch/rjp5nc/Reference_genomes/mito_reference/${folder}.bed \
-  --sample-name-map /scratch/rjp5nc/UK2022_2024/sample_map.txt \
-  --reader-threads $CPU
+--genomicsdb-workspace-path $WORKING_FOLDER/${species}/Daphnia_DBI_${species} \
+--tmp-dir $WORKING_FOLDER/TEMP_Daphnia_DBI_${species}_${sample_id} \
+--batch-size 50 \
+--sample-name-map /scratch/rjp5nc/UK2022_2024/sample_map_fixed.txt \
+--reader-threads $CPU \
+-L "${interval}"
 
 #sample name from g.vcf.gz 
 # Remove temp workspace
