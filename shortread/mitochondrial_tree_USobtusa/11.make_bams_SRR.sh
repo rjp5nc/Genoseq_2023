51ad022df75258cd9b4f55cc0a5da07869231829
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-04 21:22:55 -0400

    Update 11.make_bams_SRR.sh

--- shortread/mitochondrial_tree_USobtusa/11.make_bams_SRR.sh
+++ shortread/mitochondrial_tree_USobtusa/11.make_bams_SRR.sh
@@ -87,6 +87,6 @@ java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
 cp ${outfq}/${samp}_finalmap* ${outbam}/
 
 # Remove intermediate files
-rm -f ${outfq}/${samp}.*
+#rm -f ${outfq}/${samp}.*
 
 echo "Finished processing ${samp}"
