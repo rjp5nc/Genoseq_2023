c946b7bdec17cd49bd6c261c3cfcf3754c173c52
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-14 14:08:32 -0400

    Update 90.nflo.mag.sh

--- finalscripts_manuscript_3/90.nflo.mag.sh
+++ finalscripts_manuscript_3/90.nflo.mag.sh
@@ -25,7 +25,7 @@
 
 module load bioconda/py3.10
 
-/home/rjp5nc/bin/nextflow run nf-core/mag -profile conda --input '/project/berglandlab/Robert/HMWDNAElvis3/RawDatafromCDGenomics/fastq/m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz' --outdir /scratch/rjp5nc/mag -r 4.0.0
+/home/rjp5nc/bin/nextflow run /home/rjp5nc/.nextflow/assets/nf-core/mag/ -profile singularity --input '/project/berglandlab/Robert/HMWDNAElvis3/RawDatafromCDGenomics/fastq/m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz' --outdir /scratch/rjp5nc/mag -r 4.0.0
 
 
 #/home/rjp5nc/bin/nextflow run nf-core/mag \
