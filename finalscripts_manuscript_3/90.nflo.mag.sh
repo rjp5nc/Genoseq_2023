11df15f5af3b69b98403362d5fcb2cccb2a53d76
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-14 13:38:15 -0400

    Update 90.nflo.mag.sh

--- finalscripts_manuscript_3/90.nflo.mag.sh
+++ finalscripts_manuscript_3/90.nflo.mag.sh
@@ -3,15 +3,19 @@
 #SBATCH --ntasks=1        # Single task per job
 #SBATCH --cpus-per-task=10 # Number of CPU cores per task
 #SBATCH -N 1              # Run on one node
-#SBATCH -t 1-20:00        # 10 hours runtime
+#SBATCH -t 5-20:00        # 10 hours runtime
 #SBATCH --mem=100G        # Memory per node
-#SBATCH -o /scratch/rjp5nc/outputerrors/down.%A_%a.out  # Standard output
-#SBATCH -e /scratch/rjp5nc/outputerrors/down.%A_%a.err  # Standard error
+#SBATCH -o /scratch/rjp5nc/outputerrors/mag.%A_%a.out  # Standard output
+#SBATCH -e /scratch/rjp5nc/outputerrors/mag.%A_%a.err  # Standard error
 #SBATCH -p standard       # Partition
 #SBATCH --account=berglandlab
 
 #/home/rjp5nc/.nextflow/assets/nf-core/mag/
 
-module load nextflow
+#curl -s https://get.nextflow.io | bash
+#chmod +x nextflow
+#export PATH=~/bin:$PATH
+#nextflow -v
 
-nextflow run nf-core/mag -profile singularity --input /project/berglandlab/Robert/HMWDNAElvis3/RawDatafromCDGenomics/fastq/m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz --outdir /scratch/rjp5nc/mag
+
+/home/rjp5nc/bin/nextflow run nf-core/mag -profile singularity --input /project/berglandlab/Robert/HMWDNAElvis3/RawDatafromCDGenomics/fastq/m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz --outdir /scratch/rjp5nc/mag
