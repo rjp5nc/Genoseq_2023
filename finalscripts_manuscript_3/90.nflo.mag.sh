d329b58e41a2930de225ad4d084eca2106e2fa97
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-14 13:31:58 -0400

    up

---
+++ finalscripts_manuscript_3/90.nflo.mag.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH -J nfloMag    # Job name
+#SBATCH --ntasks=1        # Single task per job
+#SBATCH --cpus-per-task=10 # Number of CPU cores per task
+#SBATCH -N 1              # Run on one node
+#SBATCH -t 1-20:00        # 10 hours runtime
+#SBATCH --mem=100G        # Memory per node
+#SBATCH -o /scratch/rjp5nc/outputerrors/down.%A_%a.out  # Standard output
+#SBATCH -e /scratch/rjp5nc/outputerrors/down.%A_%a.err  # Standard error
+#SBATCH -p standard       # Partition
+#SBATCH --account=berglandlab
+
+#/home/rjp5nc/.nextflow/assets/nf-core/mag/
+
+module load nextflow
+
+nextflow run nf-core/mag -profile singularity --input /project/berglandlab/Robert/HMWDNAElvis3/RawDatafromCDGenomics/fastq/m84128_250121_222443_s2.hifi_reads.bc2104.fq.gz --outdir /scratch/rjp5nc/mag
--- shortread/mitochondrial_tree_USobtusa/tree_bulding.r
+++ shortread/mitochondrial_tree_USobtusa/tree_bulding.r
@@ -22,6 +22,9 @@ metadata_with_clone <- read.csv("/project/berglandlab/Robert/UKSequencing2022_20
 
 head(metadata)
 
+gds.fn <- "/scratch/rjp5nc/UK2022_2024/mito_vcf/mito_lifted_all_missingasref.gds"
+genofile <- seqOpen(gds.fn)
+
 samples_to_keep <- metadata_with_clone %>% filter(accuratelocation == "P63"|accuratelocation == "P62"|accuratelocation == "PBO66"|accuratelocation == "P58"|accuratelocation == "Gilmer"|accuratelocation == "P759") %>% pull(Well)
   
 #samples_to_keep <- metadata_with_clone %>% pull(Sample)
@@ -121,6 +124,7 @@ library(ggtree)
 library(ggplot2)
 library(dplyr)
 library(tibble)
+library(data.table)
 
 # ---- Step 1: Open GDS file ----
 
@@ -134,11 +138,18 @@ metadata_with_clone <- subset(metadata_with_clone, clone !="Blank" & clone !="BL
 
 head(metadata)
 
+#gds.fn <- "/scratch/rjp5nc/UK2022_2024/mito_vcf/usdobtusa_mito_genotyped.gds"             # output GDS file
+gds.fn <- "/scratch/rjp5nc/UK2022_2024/mito_vcf/mito_lifted_all_missingasref.gds"
+genofile <- seqOpen(gds.fn)
+
+
+
 seqResetFilter(genofile)
 
 
 samples_to_keep <- metadata_with_clone %>% filter(accuratelocation == "P63"|accuratelocation == "P62"|accuratelocation == "PBO66"|accuratelocation == "P58"|accuratelocation == "Gilmer"|accuratelocation == "P759"|accuratelocation == "Dorset") %>% pull(Well)
-samples_to_keep <- metadata_with_clone %>% filter(accuratelocation == "P759"|accuratelocation == "Dorset") %>% pull(Well)
+#samples_to_keep <- metadata_with_clone %>% filter(accuratelocation == "P759"|accuratelocation == "Dorset") %>% pull(Well)
+#samples_to_keep <- metadata_with_clone %>% filter(location == "UK" | accuratelocation == "P759") %>% pull(Well)
 
 samples_to_keep <- metadata_with_clone$Well
 
@@ -170,6 +181,9 @@ keep <- which(miss_rate < 0.05 & mean_depth > 10)
 ibs <- snpgdsIBS(genofile, sample.id = final_valid_samples, snp.id = keep, num.thread = 4, autosome.only = FALSE)
 dist_matrix <- 1 - ibs$ibs
 rownames(dist_matrix) <- colnames(dist_matrix) <- ibs$sample.id
+
+write.csv(dist_matrix, "/scratch/rjp5nc/UK2022_2024/mito_vcf/fullmito_similarity_matrix.csv")  # Save as CSV
+
 
 # ---- Step 4: Build neighbor-joining tree ----
 tree <- nj(as.dist(dist_matrix))
@@ -214,11 +228,13 @@ tip_color_dt <- data.table(
 )
 
 
-png("/scratch/rjp5nc/UK2022_2024/mito_vcf/treemitophylogram_circular_rooted_by_P759.png", width = 1200, height = 2000)
+png("/scratch/rjp5nc/UK2022_2024/mito_vcf/treemitophylogram_rectangular_all_rooted_by_P759.png", res = 300, width = 6000, height = 15000)
+#png("/scratch/rjp5nc/UK2022_2024/mito_vcf/tree_usobtusa_circ.png", width = 1200, height = 2000)
 
 # Plot the tree
 plot.phylo(tree_rooted,
            type = "phylogram",
+#           type = "fan",
            cex = 0.8,
            label.offset = 0.01,
            no.margin = TRUE,
@@ -240,3 +256,232 @@ dev.off()
 
 
 
+png("/scratch/rjp5nc/UK2022_2024/mito_vcf/treemitophylogram_fan_all_rooted_by_P759.png", res = 300, width = 9000, height = 9000)
+#png("/scratch/rjp5nc/UK2022_2024/mito_vcf/tree_usobtusa_circ.png", width = 1200, height = 2000)
+
+# Plot the tree
+plot.phylo(tree_rooted,
+           type = "fan",
+           cex = 0.8,
+           label.offset = 0.01,
+           no.margin = TRUE,
+           tip.color = tip_colors,
+           main = "Rooted NJ Tree (root = P759)")
+
+# Add legend
+legend("topleft",                   # position
+       legend = unique(tip_color_dt$sample),     # group names
+       col = unique(tip_color_dt$color),        # matching colors
+       pch = 19,                   # solid circle
+       pt.cex = 1.5,               # point size
+       cex = 1,                    # text size
+       bty = "n",                  # no box
+       title = "Sample Group")
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dist_matrix <- as.matrix(dist_matrix)  # Coerce it into a matrix
+
+dist_matrix2 <- 1 - dist_matrix
+
+
+# Create a named vector for mapping
+Sample<-metadata_with_clone$Well
+d<-as.data.table(Sample)
+#d$clone<- pca_datawithclone$clone
+d$date<- metadata_with_clone$date
+d$clone<- metadata_with_clone$clone
+
+name_map <-d
+
+name_dict <- setNames(name_map$clone, name_map$Sample)
+
+
+  # Coerce it into a matrix
+mode(dist_matrix2) <- "numeric"        # Ensure it's numeric
+
+# Check for NAs and replace them (if necessary)
+
+dist_matrix2[is.na(dist_matrix2)] <- 1  # Replace NAs with 0
+
+# Check the dimensions to ensure it's square
+if (nrow(dist_matrix2) != ncol(dist_matrix2)) {
+  stop("The similarity matrix is not square!")
+}
+
+
+
+# Step 2: Replace row names in the similarity matrix
+rownames(dist_matrix2) <- name_dict[rownames(dist_matrix2)]
+
+# Step 3: Replace column names in the similarity matrix
+colnames(dist_matrix2) <- name_dict[colnames(dist_matrix2)]
+
+# subset_matrix <- sim_matrix["Elvis_03", "Elvis_03"]
+# print(subset_matrix)
+
+row_indices <- which(rownames(dist_matrix2) == "D8.4A")
+col_indices <- which(colnames(dist_matrix2) == "Islands_02")
+
+# Remove all occurrences of "B"
+subset_matrix <- dist_matrix2[row_indices, col_indices]
+print(subset_matrix)
+
+
+pdf("/scratch/rjp5nc/UK2022_2024/mito_vcf/heatmapusobtusagilmer.pdf", width=30, height=30)
+
+
+# Plot the heatmap
+heatmap(dist_matrix2, 
+        main = "Genotype Similarity Heatmap", 
+        col = colorRampPalette(c("blue", "red"))(50), 
+        scale = "none", 
+        margins = c(8, 8))
+
+dev.off()
+
+
+
+
+
+
+
+
+
+library(phangorn)
+tree <- upgma(as.dist(dist_matrix))
+
+
+
+# ---- Step 5 (Optional): Relabel tips from metadata CSV ----
+# CSV should have columns: Sample_ID and Label (or Well, Clone, etc.)
+
+
+label_map <- setNames(metadata_with_clone$clone, metadata_with_clone$Well)
+
+# Apply relabeling
+
+# Apply labels (optional: fallback to Sample_ID if label is missing)
+tree$tip.label <- label_map[tree$tip.label]
+tree$tip.label[is.na(tree$tip.label)] <- names(label_map)[is.na(tree$tip.label)]
+
+
+
+
+group_colors <- setNames(rainbow(length(unique(metadata_with_clone$date))), unique(metadata_with_clone$date))
+
+# Assign color to each tip
+
+
+metadata_with_clone$date
+
+tip_colors <- group_colors[metadata_with_clone$date]
+
+tip_color_dt <- data.table(metadata_with_clone$date, metadata_with_clone$Well)
+
+final_valid_samples2 <- as.data.frame(final_valid_samples)
+
+final_valid_samples3 <- left_join(final_valid_samples2, tip_color_dt, by= c("final_valid_samples"="V2"))
+
+tip_colors <- group_colors[final_valid_samples3$V1]
+
+tree_rooted <- root(tree, outgroup = "P759", resolve.root = TRUE)
+
+tip_color_dt <- data.table(
+  sample = names(tip_colors),
+  color  = as.vector(tip_colors)
+)
+
+
+png("/scratch/rjp5nc/UK2022_2024/mito_vcf/treemitophylogram_rectangular_upgma_all_rooted_by_P759.png", res = 300, width = 6000, height = 15000)
+#png("/scratch/rjp5nc/UK2022_2024/mito_vcf/tree_usobtusa_circ.png", width = 1200, height = 2000)
+
+# Plot the tree
+plot.phylo(tree_rooted,
+           type = "phylogram",
+#           type = "fan",
+           cex = 0.8,
+           label.offset = 0.01,
+           no.margin = TRUE,
+           tip.color = tip_colors,
+           main = "Rooted NJ Tree (root = P759)")
+
+# Add legend
+legend("topleft",                   # position
+       legend = unique(tip_color_dt$sample),     # group names
+       col = unique(tip_color_dt$color),        # matching colors
+       pch = 19,                   # solid circle
+       pt.cex = 1.5,               # point size
+       cex = 1,                    # text size
+       bty = "n",                  # no box
+       title = "Sample Group")
+
+dev.off()
+
+
+
+
+png("/scratch/rjp5nc/UK2022_2024/mito_vcf/treemitophylogram_fan_all_rooted_by_P759.png", res = 300, width = 9000, height = 9000)
+#png("/scratch/rjp5nc/UK2022_2024/mito_vcf/tree_usobtusa_circ.png", width = 1200, height = 2000)
+
+# Plot the tree
+plot.phylo(tree_rooted,
+           type = "fan",
+           cex = 0.8,
+           label.offset = 0.01,
+           no.margin = TRUE,
+           tip.color = tip_colors,
+           main = "Rooted NJ Tree (root = P759)")
+
+# Add legend
+legend("topleft",                   # position
+       legend = unique(tip_color_dt$sample),     # group names
+       col = unique(tip_color_dt$color),        # matching colors
+       pch = 19,                   # solid circle
+       pt.cex = 1.5,               # point size
+       cex = 1,                    # text size
+       bty = "n",                  # no box
+       title = "Sample Group")
+
+dev.off()
+
+
+
+
+
+
+library(SNPRelate)
+
+
+library(SeqArray)
+library(SeqVarTools)
+
+# Create SeqVarData object
+svd <- SeqVarData(genofile)
+
+# Run LD pruning using SeqVarTools
+ld <- ldPruning(svd, window.size=500000, ld.threshold=0.8)
+
+# Extract pruned SNPs
+snpset <- unlist(ld)
+length(snpset)
