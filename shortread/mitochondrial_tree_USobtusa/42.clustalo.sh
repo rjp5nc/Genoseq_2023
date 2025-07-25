f53e1d853d65c50ddf444196760f602421a94257
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-24 21:09:29 -0400

    Update 42.clustalo.sh

--- shortread/mitochondrial_tree_USobtusa/42.clustalo.sh
+++ shortread/mitochondrial_tree_USobtusa/42.clustalo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#SBATCH -J clusterio # A single job name for the array
+#SBATCH -J clustalo # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 72:00:00 ### 15 seconds
-#SBATCH --mem 50G
+#SBATCH --mem 20G
 #SBATCH -o /scratch/rjp5nc/erroroutputs/cluster_1.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/erroroutputs/cluster_1.%A_%a.err # Standard error
 #SBATCH -p standard
@@ -15,9 +15,17 @@
 #conda create -n clustalo-env -c bioconda -c conda-forge clustalo
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate clustalo-env
+#conda install -c bioconda fasttree
 
 #mkdir -p /scratch/rjp5nc/UK2022_2024/consensusmitoaligned
 #cat /scratch/rjp5nc/UK2022_2024/consensusmito/*.fa > /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_mitosequences.fasta
 
 clustalo -i /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_mitosequences.fasta \
 -o /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_aligned.fasta --threads 10 --force
+
+FastTree -nt -gtr /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_aligned_unique.fasta > /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/mito_tree.nwk
+
+#awk 'BEGIN{n=0} /^>/ {n++} n<=100 {print}' /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_mitosequences.fasta > mito_subset.fasta
+
+# Run locally with 4 threads (or adjust to 10)
+#clustalo -i mito_subset.fasta -o mito_subset.aln --threads=4
