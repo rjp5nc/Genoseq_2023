b2ef803f9b648f348ebf540e13cd8c2766694623
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-27 19:40:47 -0400

    Create 44.treeannotator.sh

---
+++ shortread/mitochondrial_tree_USobtusa/44.treeannotator.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#SBATCH -J BEAST # A single job name for the array
+#SBATCH --ntasks-per-node=30 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 6-0:00:00 ### 15 seconds
+#SBATCH --mem 120G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/beast.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/beast.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --mail-type=END               # Send email at job completion
+#SBATCH --mail-user=rjp5nc@virginia.edu    # Email address for notifications
+
+#conda create -n beast2-277 openjdk=17 -c conda-forge -y
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate beast2-277
+
+cd /scratch/rjp5nc/UK2022_2024/consensusmitoaligned2/
+
+treeannotator -burnin 10 -heights mean all_aligned-all_aligned_unique.trees annotated_MCC.tree
