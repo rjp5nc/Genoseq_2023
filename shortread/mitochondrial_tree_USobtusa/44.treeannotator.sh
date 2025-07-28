67a383cfda157ec586ab67407ee7911264ade2db
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-07-27 20:00:29 -0400

    Update 44.treeannotator.sh

--- shortread/mitochondrial_tree_USobtusa/44.treeannotator.sh
+++ shortread/mitochondrial_tree_USobtusa/44.treeannotator.sh
@@ -18,7 +18,7 @@ conda activate beast2-277
 
 cd /scratch/rjp5nc/UK2022_2024/consensusmitoaligned2/
 
-java -Xmx16g -cp "/scratch/rjp5nc/beast/beast/lib/packages/BEAST.app.jar:/scratch/rjp5nc/beast/beast/lib/packages/BEAST.base.jar" beastfx.app.treeannotator.TreeAnnotator \
+java -Xmx16g -cp "/scratch/rjp5nc/beast/beast/lib/packages/*" beastfx.app.treeannotator.TreeAnnotator \
   -burnin 10 \
   -height mean \
   all_aligned-all_aligned_unique.trees \
