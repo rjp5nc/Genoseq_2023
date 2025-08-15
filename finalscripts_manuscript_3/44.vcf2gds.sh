5a656ef290c775c61c2c36ee53b26ad0a089cb9b
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-08-15 13:41:34 -0400

    Update 44.vcf2gds.sh

--- finalscripts_manuscript_3/44.vcf2gds.sh
+++ finalscripts_manuscript_3/44.vcf2gds.sh
@@ -3,7 +3,7 @@
 #SBATCH -J vcf2gds # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-48:00  ### 48 hours
+#SBATCH -t 0-10:00  ### 48 hours
 #SBATCH --mem 60G
 #SBATCH -o /scratch/rjp5nc/vcf2gds.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/vcf2gds.%A_%a.err # Standard error
