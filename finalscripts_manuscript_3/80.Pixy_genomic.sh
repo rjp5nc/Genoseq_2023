2943ec112051a4d653d8cd42ed9618f37cd63ad8
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-16 09:30:06 -0400

    Update 80.Pixy_genomic.sh

--- finalscripts_manuscript_3/80.Pixy_genomic.sh
+++ finalscripts_manuscript_3/80.Pixy_genomic.sh
@@ -60,32 +60,24 @@ cut -f1 pops_fixed.txt > pops_samples.txt
 # Keep only matching samples
 grep -F -x -f vcf_samples.txt pops_samples.txt > pops_samples_filtered.txt
 
-
-#bgzip -@ 16 -c trimmed10bp_allsites_usobtusa.vcf.gz > trimmed10bp_allsites_usobtusa.bgz
-
-# rename to standard convention
-mv trimmed10bp_allsites_usobtusa.bgz trimmed10bp_allsites_usobtusa_bgz.vcf.gz
-# index for random access
-tabix -p vcf trimmed10bp_allsites_usobtusa_bgz.vcf.gz
-
 bcftools view -S pops_samples_filtered.txt -Oz \
     --threads 16 \
-    -o trimmed10bp_allsites_usobtusa.filtered_bgz.vcf.gz \
-    trimmed10bp_allsites_usobtusa_bgz.vcf.gz
+    -o trimmed10bp_allsites_usobtusa.filtered.vcf.gz \
+    trimmed10bp_allsites_usobtusa.vcf.gz
 
 tabix -p vcf trimmed10bp_allsites_usobtusa.filtered.vcf.gz
 
-bcftools index -t trimmed10bp_allsites_usobtusa.filtered_bgz.vcf.gz
-bcftools query -l trimmed10bp_allsites_usobtusa.filtered_bgz.vcf.gz > filtered_samples.txt
+bcftools index -t trimmed10bp_allsites_usobtusa.filtered.vcf.gz
+bcftools query -l trimmed10bp_allsites_usobtusa.filtered.vcf.gz > filtered_samples.txt
 grep -Ff filtered_samples.txt pops_fixed.txt > pops_filtered_for_pixy.txt
 
-bcftools query -l trimmed10bp_allsites_usobtusa.filtered_bgz.vcf.gz > vcf_samples2.txt
+bcftools query -l trimmed10bp_allsites_usobtusa.filtered.vcf.gz > vcf_samples2.txt
 awk -F'\t' 'NR==FNR {vcf[$1]; next} $1 in vcf' vcf_samples2.txt pops_filtered_for_pixy.txt > pops_pixy_ready.txt
 
 
 
 pixy --stats pi fst dxy \
---vcf trimmed10bp_allsites_usobtusa.filtered_bgz.vcf.gz \
+--vcf trimmed10bp_allsites_usobtusa.filtered.vcf.gz \
 --populations pops_pixy_ready.txt \
 --window_size 10000 \
 --n_cores 16 \
