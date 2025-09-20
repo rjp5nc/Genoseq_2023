33b7f1bcdf6c4fc401e0d19211d598d137bae891
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-19 23:28:43 -0400

    Create 11.merge.sh

---
+++ microbiota/11.merge.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J merge # A single job name for the array
+#SBATCH --ntasks-per-node=10 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 2-10:00 # 10 hours
+#SBATCH --mem 100G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/down.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/down.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+#cp -r /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/Rockpool2_B3/ /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/testing/
+# Set the parent directory containing all the folders
+PARENT_DIR="/scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/fastqs"  # Change this to your actual path
+cd "$PARENT_DIR" || exit
+
+# Loop through all subdirectories
+ls *trimmed1.fq.gz 1> /dev/null 2>&1; then
+        cat *trimmed1.fq.gz > "unmapped_trimmedmerged1.fq.gz"
+
+    # Check for reverse reads
+ls *trimmed2.fq.gz 1> /dev/null 2>&1; then
+        cat *trimmed2.fq.gz > "unmapped_trimmedmerged2.fq.gz"
+
+
+
+
+
+
+
+
+#for folder in */; do
+ #   folder_name="${folder%/}"  # Remove trailing slash from folder name
+  #  echo "Entering folder: $folder_name"
+   # cd "$folder_name" || continue  # Enter folder
+
+    # Check for forward reads
+    #if ls *.fastq.gz 1> /dev/null 2>&1; then
+     #   echo "Merging all forward reads in $folder_name..."
+# Trim illumina adaptors
+
+
+
