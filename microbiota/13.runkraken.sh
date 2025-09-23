ea9fcda00b552cb4748d648eb05e3cdc1ecab505
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-22 22:22:13 -0400

    Update 13.runkraken.sh

--- microbiota/13.runkraken.sh
+++ microbiota/13.runkraken.sh
@@ -19,16 +19,12 @@ export kraken2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
 #echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
 #source ~/.bashrc
 
-cd /scratch/rjp5nc/krakenDB/nt
-tar -xvzf k2_core_nt_20241228.tar.gz
+#cd /scratch/rjp5nc/krakenDB/nt
+#tar -xvzf k2_core_nt_20241228.tar.gz
 
 cd /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/
 
-kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 10 \
---report /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_report.txt \
---classified-out /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_classified_output.txt \
---output /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_output.txt \
---use-names /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k.fasta
+kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 10 --report /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_report.txt --classified-out /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_classified_output.txt --output /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_output.txt --use-names /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k.fasta
 
 grep '^>' /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_classified_output.txt > /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_classified_headers.txt
 
