0c9854ba7cc6789618ac9d6fdef9f5dff94deba2
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-23 20:17:25 -0400

    Update 89.VCF_to_fasta.sh

--- finalscripts_manuscript_3/89.VCF_to_fasta.sh
+++ finalscripts_manuscript_3/89.VCF_to_fasta.sh
@@ -3,7 +3,7 @@
 #SBATCH -J trim # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-92:00  ### 48 hours
+#SBATCH -t 0-10:00  ### 48 hours
 #SBATCH --mem 50G
 #SBATCH -o /scratch/rjp5nc/err/trim.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/err/trim.%A_%a.err # Standard error
