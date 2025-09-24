a708f49a840b4a65f89d564410b2f6bc04dfe71b
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-24 10:11:36 -0400

    Update 995.vcf2gds_indv.sh

--- finalscripts_manuscript_3/995.vcf2gds_indv.sh
+++ finalscripts_manuscript_3/995.vcf2gds_indv.sh
@@ -16,6 +16,11 @@
 module load gcc openmpi R/4.3.1
 
 
+#bcftools view -h trimmed10bp_filtered_two_of_each.vcf.gz > header.txt
+#Find the line like:
+##INFO=<ID=MLEAC,Number=A,Type=Integer,Description="...">
+#Edit it to:
+##INFO=<ID=MLEAC,Number=.,Type=Integer,Description="...">
 #bcftools reheader -h header.txt -o trimmed10bp_filtered_two_of_each_fixed.vcf.gz trimmed10bp_filtered_two_of_each.vcf.gz
 #bcftools index trimmed10bp_filtered_two_of_each_fixed.vcf.gz
 
