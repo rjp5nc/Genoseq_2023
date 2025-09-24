6ffdb97100ff384886936b076849dc7c8781bed9
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-23 20:37:24 -0400

    Update 90.fasta_to_nexus.sh

--- finalscripts_manuscript_3/90.fasta_to_nexus.sh
+++ finalscripts_manuscript_3/90.fasta_to_nexus.sh
@@ -9,26 +9,9 @@
 #SBATCH -e /scratch/rjp5nc/err/trim.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
-#SBATCH --array=1-<NUM_SAMPLES>
 
-#NUM_SAMPLES=$(wc -l < /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/final_vcf_filter_two_of_each.txt)
-# submit SLURM array job
-#sbatch --array=1-${NUM_SAMPLES} 89.VCF_to_fasta.sh
 
-module load bcftools
+cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/
 
-#SLURM_ARRAY_TASK_ID=1
-
-VCF="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_filtered_two_of_each.vcf.gz"
-REF="/scratch/rjp5nc/Reference_genomes/post_kraken/First_12_US_obtusa_onlydaps.fasta"
-SAMPLE_LIST="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/final_vcf_filter_two_of_each.txt"
-OUTDIR="/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/consensus_fastas"
-
-# make sure output directory exists
-mkdir -p "$OUTDIR"
-
-# get the sample name corresponding to this array task
-SAMPLE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_LIST")
-
-# run bcftools consensus
-bcftools consensus -f "$REF" -s "$SAMPLE" -H I "$VCF" > "$OUTDIR/${SAMPLE}.fasta"
+cat consensus_fastas/*.fasta > all_samples.fasta
+python /scratch/rjp5nc/AMAS/amas/AMAS.py convert -i all_samples.fasta -f fasta -d dna -o all_samples.nex
