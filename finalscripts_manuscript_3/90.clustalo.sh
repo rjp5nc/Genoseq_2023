7fbfe65e32c725b12c6629d6f8648a20db663a33
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-23 21:15:15 -0400

    Create 90.clustalo.sh

---
+++ finalscripts_manuscript_3/90.clustalo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#SBATCH -J clustalo # A single job name for the array
+#SBATCH --ntasks-per-node=10 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 72:00:00 ### 15 seconds
+#SBATCH --mem 20G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/cluster_1.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/cluster_1.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --mail-type=END               # Send email at job completion
+#SBATCH --mail-user=rjp5nc@virginia.edu    # Email address for notifications
+
+#conda create -n clustalo-env -c bioconda -c conda-forge clustalo
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate clustalo-env
+#conda install -c bioconda fasttree
+
+cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/
+
+# cat consensus_fastas/*.fasta > only2each.fasta
+
+clustalo -i only2each.fasta \
+-o only2each_aligned.fasta --threads 10 --force
+
+awk '/^>/{count[$0]++; if(count[$0]>1) $0=$0"_"count[$0]; print; next}1' \
+only2each_aligned.fasta \
+> only2each_aligned_unique.fasta
+
+#FastTree -nt -gtr /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_aligned_unique.fasta > /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/mito_tree.nwk
+
+#awk 'BEGIN{n=0} /^>/ {n++} n<=100 {print}' /scratch/rjp5nc/UK2022_2024/consensusmitoaligned/all_mitosequences.fasta > mito_subset.fasta
+
