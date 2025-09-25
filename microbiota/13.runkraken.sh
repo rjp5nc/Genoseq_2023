b6d545c3ac59e2e7c16924ec05c175ebee2994cc
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-25 13:27:20 -0400

    Update 13.runkraken.sh

--- microbiota/13.runkraken.sh
+++ microbiota/13.runkraken.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J run_kraken # A single job name for the array
-#SBATCH --ntasks-per-node=30 # multi core
+#SBATCH --ntasks-per-node=10 # multi core
 #SBATCH -N 1 # on one node
 #SBATCH -t 1-03:00 # 3 hours
-#SBATCH --mem 150G
+#SBATCH --mem 300G
 #SBATCH -o /scratch/rjp5nc/err/kraken%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/err/kraken%A_%a.err # Standard error
 #SBATCH -p standard
@@ -13,11 +13,11 @@
 module load kraken2
 
 export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
-#export kraken2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
-#KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
+export kraken2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
+KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
 
-#echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
-#source ~/.bashrc
+echo 'export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"' >> ~/.bashrc
+source ~/.bashrc
 
 #cd /scratch/rjp5nc/krakenDB/nt
 #tar -xvzf k2_core_nt_20241228.tar.gz
@@ -26,7 +26,7 @@ export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
 cd /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/
 
 kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt \
-  --threads 28 \
+  --threads 10 \
   --use-names \
   --report scaffolds_report.txt \
   --output scaffolds_output.txt \
