6a4324298e3de0dd22a01f641456a02ca7183f4c
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-25 21:10:34 -0400

    up

--- microbiota/13.runkraken.sh
+++ microbiota/13.runkraken.sh
@@ -21,24 +21,24 @@ export KRAKEN2_DATA_PATH="/scratch/rjp5nc/krakenDB/nt"
 #tar -xvzf k2_core_nt_20241228.tar.gz
 #kraken2-build --build --threads 10 --db /scratch/rjp5nc/krakenDB/nt
 
-#kraken2 --db /scratch/rjp5nc/krakenDB/nt \
-#--threads 4 --report /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/report.txt \
-#--classified-out /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_classified_output.txt \
-#--output /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_output.txt \
-#--use-names \
-#/scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k.fasta
+kraken2 --db /scratch/rjp5nc/krakenDB/nt \
+--threads 4 --report /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/report.txt \
+--classified-out /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_classified_output.txt \
+--output /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_output.txt \
+--use-names \
+/scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k.fasta
 
 #kraken2 --memory-mapping --db /scratch/rjp5nc/krakenDB/nt --threads 10 --report /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_report.txt --classified-out /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_classified_output.txt --output /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_output.txt --use-names /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k.fasta
 
-#grep '^>' /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_kraken_classified_output.txt > /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/scaffolds_min10k_classified_headers.txt
+grep '^>' /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_classified_output.txt > /scratch/rjp5nc/UK2022_2024/unmapped_fastqs_newseq/SPADES_norm/kraken_classified_headers_output.txt
 
 
 #kraken2-build --standard --db /scratch/rjp5nc/krakenDB/test2
 
 
-kraken2 --db /scratch/rjp5nc/krakenDB/nt \
---threads 4 --report /project/berglandlab/chlorella_sequencing/chlorella_ref_kraken_report.txt \
---classified-out /project/berglandlab/chlorella_sequencing/kraken_chlorella_ref_classified_output.txt \
---output /project/berglandlab/chlorella_sequencing/chlorella_ref_kraken_output.txt \
---use-names \
-/project/berglandlab/chlorella_sequencing/reference_genome/GCA_023343905.1_cvul_genomic.fa
+#kraken2 --db /scratch/rjp5nc/krakenDB/nt \
+#--threads 4 --report /project/berglandlab/chlorella_sequencing/chlorella_ref_kraken_report.txt \
+#--classified-out /project/berglandlab/chlorella_sequencing/kraken_chlorella_ref_classified_output.txt \
+#--output /project/berglandlab/chlorella_sequencing/chlorella_ref_kraken_output.txt \
+#--use-names \
+#/project/berglandlab/chlorella_sequencing/reference_genome/GCA_023343905.1_cvul_genomic.fa
