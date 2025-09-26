0b1d9fb6453e3cc9d62b58660293673073c45fea
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-25 20:59:28 -0400

    Create 13.modkraken.sh

---
+++ microbiota/13.modkraken.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J run_kraken # A single job name for the array
+#SBATCH --ntasks-per-node=10 # multi core
+#SBATCH -N 1 # on one node
+#SBATCH -t 3-03:00 # 3 hours
+#SBATCH --mem 100G
+#SBATCH -o /scratch/rjp5nc/err/cilliates%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/err/cilliates%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+cd /scratch/rjp5nc/krakenDB/cilliates
+
+esearch -db nucleotide -query "Vorticella[Organism]" | efetch -format fasta > vorticella_only.fasta
+
+# --- Add sequences to the Kraken2 library ---
+kraken2-build --add-to-library vorticella_only.fasta --db /scratch/rjp5nc/krakenDB/nt
+kraken2-build --add-to-library cilliates.fasta --db /scratch/rjp5nc/krakenDB/nt
+# --- Rebuild the Kraken2 database ---
+kraken2-build --build --db /scratch/rjp5nc/krakenDB/nt --threads 10
