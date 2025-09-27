c2b03cfe3250be90aa08ce5749d9d1d14a4e4b44
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-27 12:37:55 -0400

    Update 86.Avg_depth.sh

--- finalscripts_manuscript_3/86.Avg_depth.sh
+++ finalscripts_manuscript_3/86.Avg_depth.sh
@@ -19,66 +19,15 @@ module load bcftools
 
 VCF=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_filtered_one_of_each.vcf.gz
 
- bcftools query -l /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_filtered_one_of_each.vcf.gz \
- > samples.txt
+ bcftools query -l /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_filtered_one_of_each.vcf.gz > samples.txt
 nsamples=$(bcftools query -l "$VCF" | wc -l)
+echo "Samples: $nsamples"
 
 samples_str=$(bcftools query -l "$VCF" | paste -sd "," -)
 echo "Samples: $samples_str"
 
-# bcftools index -s /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz \
-# | cut -f1 > contigs.txt
-
-# sed -i 's/\r//g; s/ //g' contigs.txt
-
-# cut -f1 contigs.txt > contigs_only.txt
-# grep -v "^$" contigs_only.txt > contigs_clean.txt
-
 RESULTDIR=dp_results_one_of_each
 mkdir -p "$RESULTDIR"
-
-# bcftools query -f '%CHROM\t%POS[\t%DP]\n' -r "$contig" "$VCF" \
-# | awk -v nsamples="$nsamples" -v contig="$contig" '
-# {
-#     win = int($2/10000)
-#     for(i=3;i<=NF;i++){
-#         key=contig":"win":"i
-#         sum[key] += ($i!=".") ? $i : 0
-#         count[key] += ($i!=".")
-#     }
-# }
-# END {
-#     for(k in sum){
-#         split(k,a,":")
-#         win_start = a[2]*10000
-#         win_end = win_start + 9999
-#         sample_name = ENVIRON["samples" a[3]-3]
-#         avg = (count[k]>0) ? sum[k]/count[k] : 0
-#         printf "%s\t%d\t%d\t%s\t%.2f\n", a[1], win_start, win_end, sample_name, avg
-#     }
-# }' \
-# | sort -k2,2n -k4,4 \
-# > "$RESULTDIR/${contig}.avgdepth.long.sorted.txt"
-
-#cat *.avgdepth.long.sorted.txt > ../avg.depth.all12.txt
-
-
-# awk '
-# {
-#     key = $1"\t"$2"\t"$3
-#     sum[key] += $5
-#     count[key] += 1
-# }
-# END {
-#     OFS="\t"
-#     print "contig","window_start","window_end","avg_depth"
-#     for(k in sum){
-#         avg = sum[k]/count[k]
-#         print k, avg
-#     }
-# }
-# ' avg.depth.all12.txt > window_avg_depth.txt
-
 
 # Get contig for this array task
 contig=$(sed -n "${SLURM_ARRAY_TASK_ID}p" contigs_clean.txt)
@@ -88,22 +37,24 @@ if [[ -z "$contig" ]]; then
 fi
 echo "Processing contig: $contig"
 
-# Main depth extraction + window averaging
 bcftools query -f '%CHROM\t%POS[\t%DP]\n' -r "$contig" "$VCF" \
 | awk -v nsamples="$nsamples" -v contig="$contig" -v SAMPLES="$samples_str" '
 BEGIN { split(SAMPLES, sample_arr, ",") }
 {
     win = int($2/100000)  # 100kb windows
     for(i=1; i<=nsamples; i++){
-        dp = $(i+2)  # offset: $1=CHROM, $2=POS
-        sum[i,win]   += (dp ~ /^[0-9.]+$/ && dp != ".") ? dp : 0
-        count[i,win] += (dp ~ /^[0-9.]+$/ && dp != ".") ? 1 : 0
+        dp = $(i+2)
+        if(dp ~ /^[0-9]+$/){      # only add numeric DP
+            sum[i,win]   += dp
+            count[i,win] += 1
+        }
+        seen[i,win] = 1            # mark this window as seen, even if all DP are missing
     }
 }
 END {
     for(i=1; i<=nsamples; i++){
         sample_name = sample_arr[i]
-        for(win_key in sum){
+        for(win_key in seen){
             split(win_key, w, ",")
             if(w[1]==i){
                 win = w[2]
@@ -121,7 +72,7 @@ END {
 
 
 
-# cat *.long.sorted_100000.txt > ../avg.depth.only1each_12_100000.txt
+# cat *avgdepth.long.sorted_100000.txt > ../avg_avgdepth.long.sorted_100000.txt
 
 # awk '
 # {
