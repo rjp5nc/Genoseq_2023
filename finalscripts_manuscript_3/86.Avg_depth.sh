c52bdca50ab7439f3faeccb7d27618ec64b363c0
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-27 10:51:08 -0400

    Update 86.Avg_depth.sh

--- finalscripts_manuscript_3/86.Avg_depth.sh
+++ finalscripts_manuscript_3/86.Avg_depth.sh
@@ -13,21 +13,23 @@
 
 cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/
 
+
+
 #cut -f1 contigs.txt > contigs_only.txt
 #grep -v "^$" contigs_only.txt > contigs_clean.txt
 
 module load bcftools
 
-VCF=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz2.vcf.gz
+VCF=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz
 
-bcftools query -l /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz2.vcf.gz \
+bcftools query -l /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz \
 > samples.txt
 
-bcftools index -s /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz2.vcf.gz \
+bcftools index -s /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz \
 | cut -f1 > contigs.txt
 
 sed -i 's/\r//g; s/ //g' contigs.txt
-VCF=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz2.vcf.gz
+VCF=/scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz
 RESULTDIR=results
 mkdir -p "$RESULTDIR"
 
@@ -44,10 +46,60 @@ for i in "${!samples[@]}"; do
     export "samples$i=${samples[$i]}"
 done
 
+# bcftools query -f '%CHROM\t%POS[\t%DP]\n' -r "$contig" "$VCF" \
+# | awk -v nsamples="$nsamples" -v contig="$contig" '
+# {
+#     win = int($2/10000)
+#     for(i=3;i<=NF;i++){
+#         key=contig":"win":"i
+#         sum[key] += ($i!=".") ? $i : 0
+#         count[key] += ($i!=".")
+#     }
+# }
+# END {
+#     for(k in sum){
+#         split(k,a,":")
+#         win_start = a[2]*10000
+#         win_end = win_start + 9999
+#         sample_name = ENVIRON["samples" a[3]-3]
+#         avg = (count[k]>0) ? sum[k]/count[k] : 0
+#         printf "%s\t%d\t%d\t%s\t%.2f\n", a[1], win_start, win_end, sample_name, avg
+#     }
+# }' \
+# | sort -k2,2n -k4,4 \
+# > "$RESULTDIR/${contig}.avgdepth.long.sorted.txt"
+
+#cat *.avgdepth.long.sorted.txt > ../avg.depth.all12.txt
+
+
+# awk '
+# {
+#     key = $1"\t"$2"\t"$3
+#     sum[key] += $5
+#     count[key] += 1
+# }
+# END {
+#     OFS="\t"
+#     print "contig","window_start","window_end","avg_depth"
+#     for(k in sum){
+#         avg = sum[k]/count[k]
+#         print k, avg
+#     }
+# }
+# ' avg.depth.all12.txt > window_avg_depth.txt
+
+
+
+
+
+
+
+
+
 bcftools query -f '%CHROM\t%POS[\t%DP]\n' -r "$contig" "$VCF" \
 | awk -v nsamples="$nsamples" -v contig="$contig" '
 {
-    win = int($2/10000)
+    win = int($2/100000)
     for(i=3;i<=NF;i++){
         key=contig":"win":"i
         sum[key] += ($i!=".") ? $i : 0
@@ -57,13 +109,30 @@ bcftools query -f '%CHROM\t%POS[\t%DP]\n' -r "$contig" "$VCF" \
 END {
     for(k in sum){
         split(k,a,":")
-        win_start = a[2]*10000
-        win_end = win_start + 9999
+        win_start = a[2]*100000
+        win_end = win_start + 99999
         sample_name = ENVIRON["samples" a[3]-3]
         avg = (count[k]>0) ? sum[k]/count[k] : 0
         printf "%s\t%d\t%d\t%s\t%.2f\n", a[1], win_start, win_end, sample_name, avg
     }
 }' \
 | sort -k2,2n -k4,4 \
-> "$RESULTDIR/${contig}.avgdepth.long.sorted.txt"
+> "$RESULTDIR/${contig}.avgdepth.long.sorted_100000.txt"
 
+#cat *.avgdepth.long.sorted_100000.txt > ../avg.depth.all12_100000.txt
+
+# awk '
+# {
+#     key = $1"\t"$2"\t"$3
+#     sum[key] += $5
+#     count[key] += 1
+# }
+# END {
+#     OFS="\t"
+#     print "contig","window_start","window_end","avg_depth"
+#     for(k in sum){
+#         avg = sum[k]/count[k]
+#         print k, avg
+#     }
+# }
+# ' avg.depth.all12_100000.txt > window_avg_depth_100000.txt
