6c0c8acc5ceaba47c914efa95a325489182dce9f
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-09-29 13:49:12 -0400

    up

---
+++ finalscripts_manuscript_3/100.Gilmer_superclone_heterozygosity.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#SBATCH -J superclone_filt # A single job name for the array
+#SBATCH --ntasks-per-node=2 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 6-0:00:00 ### 15 seconds
+#SBATCH --mem 90G
+#SBATCH -o /scratch/rjp5nc/erroroutputs/beast.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/erroroutputs/beast.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --mail-type=END               # Send email at job completion
+#SBATCH --mail-user=rjp5nc@virginia.edu    # Email address for notifications
+
+module load vcftools
+
+cd /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/heterozygoteall
+
+vcftools --gzvcf /scratch/rjp5nc/UK2022_2024/daphnia_phylo/usdobtusa_indv/trimmed10bp_allsites_Repeatmasked_usobtusa.filtered_bgz.vcf.gz \
+  --keep onlygilmer.txt \
+  --het \
+  --out sample_het
+
+
