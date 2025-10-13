c4b5d1110594da1f73433049eaa183eb395292ed
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-10-13 13:53:44 -0400

    Update 186.Avg_depth100k_unfiltered.sh

--- finalscripts_manuscript_3/186.Avg_depth100k_unfiltered.sh
+++ finalscripts_manuscript_3/186.Avg_depth100k_unfiltered.sh
@@ -9,8 +9,9 @@
 #SBATCH -e /scratch/rjp5nc/err/pixy.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
-#SBATCH --array=3
+#SBATCH --array=5
 
+##5, 9-11, 17-24, 41, 43
 
 ##SBATCH --array=1-96
 
