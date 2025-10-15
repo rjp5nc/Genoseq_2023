f9533ccf40179237231f89f612cbcaa50b1add3e
rjp5nc <43250118+rjp5nc@users.noreply.github.com>
2025-10-15 11:25:05 -0400

    up

--- finalscripts_manuscript_3/179.lastz.sh
+++ finalscripts_manuscript_3/179.lastz.sh
@@ -3,7 +3,7 @@
 #SBATCH -J lastz # A single job name for the array
 #SBATCH --ntasks-per-node=40 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 20:00:00 ### 15 seconds
+#SBATCH -t 6-00:00:00 ### 15 seconds
 #SBATCH --mem 120G
 #SBATCH -o /scratch/rjp5nc/erroroutputs/nFlo_1.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/erroroutputs/nFlo_1.%A_%a.err # Standard error
