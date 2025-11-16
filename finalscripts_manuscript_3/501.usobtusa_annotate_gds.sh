374fd057c129fd95423fc5b5e4a0085adea07533
rjp5nc <rjp5nc@virginia.edu>
2025-11-16 13:57:45 -0500

    Update 501.usobtusa_annotate_gds.sh

--- finalscripts_manuscript_3/501.usobtusa_annotate_gds.sh
+++ finalscripts_manuscript_3/501.usobtusa_annotate_gds.sh
@@ -3,7 +3,7 @@
 #SBATCH -J stats # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-30:00  ### 48 hours
+#SBATCH -t 1-30:00  ### 48 hours
 #SBATCH --mem 100G
 #SBATCH -o /scratch/rjp5nc/err/stats.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/err/stats.%A_%a.err # Standard error
