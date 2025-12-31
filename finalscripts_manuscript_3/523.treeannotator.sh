ec5865ee21889050cb8db6bf11ec4744c48c2814
rjp5nc <rjp5nc@virginia.edu>
2025-12-30 22:06:02 -0800

    Update 523.treeannotator.sh

--- finalscripts_manuscript_3/523.treeannotator.sh
+++ finalscripts_manuscript_3/523.treeannotator.sh
@@ -3,7 +3,7 @@
 #SBATCH -J treeannotator # A single job name for the array
 #SBATCH --ntasks-per-node=30 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 6-0:00:00 ### 15 seconds
+#SBATCH -t 1-0:00:00 ### 15 seconds
 #SBATCH --mem 120G
 #SBATCH -o /scratch/rjp5nc/erroroutputs/beast.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/erroroutputs/beast.%A_%a.err # Standard error
