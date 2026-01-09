8077f2d22887e6fc6db65de3f5978d8d40bd7282
rjp5nc <rjp5nc@virginia.edu>
2026-01-09 11:40:35 -0500

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J move # A single job name for the array
+#SBATCH --ntasks-per-node=10 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 0-48:00  ### 48 hours
+#SBATCH --mem 60G
+#SBATCH -o /scratch/rjp5nc/move.%A_%a.out # Standard output
+#SBATCH -e /scratch/rjp5nc/move.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+
+mkdir -p /scratch/rjp5nc/rawdata
+cp -r /project/berglandlab/Robert/shortread_data/data/01.RawData/* /scratch/rjp5nc/rawdata/
