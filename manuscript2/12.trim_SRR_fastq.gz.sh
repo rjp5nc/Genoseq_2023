36f592d313c641d8d5c26b1749b3b371458937ce
rjp5nc <rjp5nc@virginia.edu>
2026-01-09 19:12:27 -0500

    Update 12.trim_SRR_fastq.gz.sh

--- manuscript2/12.trim_SRR_fastq.gz.sh
+++ manuscript2/12.trim_SRR_fastq.gz.sh
@@ -3,7 +3,7 @@
 #SBATCH -J trimSRA # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-10:00 # 10 hours
+#SBATCH -t 0-10:00:00 # 10 hours
 #SBATCH --mem 100G
 #SBATCH -o /scratch/rjp5nc/erroroutputs/down.%A_%a.out # Standard output
 #SBATCH -e /scratch/rjp5nc/erroroutputs/down.%A_%a.err # Standard error
